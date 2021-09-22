c304e6468e4f7e9206b04f88eda57460f94a4198
Christian Unigarro <christian.unigarro@zinobe.com>
2021-09-22 16:57:59 -0500

    Styles added to lit element component.

--- final/04-lit-components/src/my-element.ts
+++ final/04-lit-components/src/my-element.ts
@@ -2,6 +2,32 @@ import { LitElement, html, customElement, property, css } from 'lit-element'
 
 @customElement('my-element')
 export class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      font-family: "Helvetica Neue", sans-serif;
+      font-size: 16px;
+    }
+
+    .highlight {
+      font-size: 30px;
+      display: inline-block;
+      animation: highlight .2s infinite;
+      font-weight: bold;
+    }
+
+    @keyframes highlight {
+      0% {
+        color: orange;
+      }
+      50% {
+        color: yellow;
+      }
+      100% {
+        color: orange;
+      }
+    }
+  `;
+
   @property()
   targetDate = '2021-09-24';
 
