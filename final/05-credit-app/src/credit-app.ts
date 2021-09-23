85698a609887b5185531b9c35ad309fd777cbfbb
Christian Unigarro <cunigarrod@gmail.com>
2021-09-22 21:17:43 -0500

    Better name for app.

--- final/05-credit-app/src/credit-app.ts
+++ final/05-credit-app/src/credit-app.ts
@@ -1,13 +1,7 @@
 import { LitElement, html, customElement, property, css } from 'lit-element'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('credit-app')
+export class CreditApp extends LitElement {
   static styles = css`
     :host {
       display: block;
@@ -17,15 +11,9 @@ export class MyElement extends LitElement {
     }
   `
 
-  /**
-   * The name to say "Hello" to.
-   */
   @property()
   name = 'World'
 
-  /**
-   * The number of times the button has been clicked.
-   */
   @property({ type: Number })
   count = 0
 
@@ -50,6 +38,6 @@ export class MyElement extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    'credit-app': CreditApp
   }
 }
