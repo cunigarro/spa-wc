bfc54abb0d8a1dde69e8f000cab7b7607ad52332
Christian Unigarro <cunigarrod@gmail.com>
2021-09-22 22:53:24 -0500

    Lazy loading added to the components loading.

--- final/05-credit-app/src/credit-app.ts
+++ final/05-credit-app/src/credit-app.ts
@@ -1,6 +1,5 @@
 import { LitElement, html, customElement, property } from 'lit-element'
-import './pages/login';
-import './pages/register';
+import { directive } from 'lit-html';
 
 @customElement('credit-app')
 export class CreditApp extends LitElement {
@@ -17,10 +16,22 @@ export class CreditApp extends LitElement {
     this._onNavigate(window.location.pathname);
   }
 
+  resolved = new WeakSet();
+
+  lazyLoad = directive((importPromise: any, template: any) => {
+    return (part: any) => {
+      if(!this.resolved.has(part)) {
+        importPromise.then(() => this.resolved.add(part));
+      }
+      part.setValue(template);
+    }
+  });
+
   _renderCurrentView() {
+    console.log(this.currentView);
     switch (this.currentView) {
-      case '/' : return html`<credit-login></credit-login>`;
-      case '/register' : return html`<credit-register></credit-register>`;
+      case '/' : return this.lazyLoad(import('./pages/login'), html`<credit-login></credit-login>`);
+      case '/register' : return this.lazyLoad(import('./pages/register'), html`<credit-register></credit-register>`);
     }
   }
 
