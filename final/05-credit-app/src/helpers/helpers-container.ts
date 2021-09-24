9734f64c554ba5b0f8fc2768cc9153b478739703
Christian Unigarro <cunigarrod@gmail.com>
2021-09-24 07:31:44 -0500

    Provider build.

--- final/05-credit-app/src/helpers/helpers-container.ts
+++ final/05-credit-app/src/helpers/helpers-container.ts
@@ -1,4 +1,5 @@
 import LazyLoading from "./lazy-loading";
+import Provider from "./provider";
 import Requester from "./requester";
 import Routing from "./routing"
 
@@ -6,11 +7,13 @@ export const Helpers = (Base: any) => class extends Base {
   routing: Routing;
   lazyLoading: LazyLoading;
   requester: Requester;
+  provider: Provider;
 
   constructor() {
     super();
     this.routing = new Routing();
     this.requester = new Requester;
     this.lazyLoading = new LazyLoading();
+    this.provider = new Provider();
   }
 }
---
+++ final/05-credit-app/src/helpers/provider.ts
@@ -0,0 +1,15 @@
+export default class Provider {
+  _instances = new Map();
+
+  constructor() {
+    if (this.addEventListener !== undefined) {
+      this.addEventListener('request-instance', (event: Event) => {
+        const key = event.detail.key;
+        if(this._instances.has(key)) {
+          event.detail.instance = this._instances.get(key);
+          event.stopPropagation();
+        }
+      });
+    }
+  }
+}
