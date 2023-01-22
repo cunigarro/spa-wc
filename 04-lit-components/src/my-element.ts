99d6dcd09e47d59c53890afe5221138ff973debb
Christian Unigarro <cunigarrod@gmail.com>
2023-01-21 19:05:30 -0500

    Lit element component changed according to javascript reactivity example.

--- 04-lit-components/src/my-element.ts
+++ 04-lit-components/src/my-element.ts
@@ -45,31 +45,31 @@ export class MyElement extends LitElement {
   render() {
     const days = Math.round(((new Date(this.targetDate) as any) - (new Date() as any)) / (1000 * 60 * 60 * 24)) + 1;
 
-    const copies = days > 0 ? html`
+    const copy = days > 0 ? html`
       <p>
         ${days == 1 ? 'Falta' : 'Faltan'}
         <span class=${days < 15 ? 'highlight' : ''}>${days}</span>
         ${days == 1 ? 'día' : 'días'}
-        para que lleguen las ${this.motive}.
+        para l@s ${this.motive}.
       </p>
     ` : days == 0 ? html`
       <p class="highlight">
-        Hoy son mis ${this.motive} hi*#@#¢∞#ta!!!
+        Hoy son mis ${this.motive}, eeeeh :)
       </p>
     ` : days < 0 ? html`
       <p>
-        Ayer fueron mis ${this.motive} hi*#@#¢∞#ta.
+        ${days == -1 ? 'Ayer' : 'Hace ' + Math.abs(days) + ' días' } fueron mis ${this.motive}, aaaah :(
       </p>
     ` : '';
 
     return html`
-      ${copies}
       <p>
         Mis
         <input type="text" value=${this.motive} @keyup=${this.onMotiveChanged} />
-        empiezan en
+        ${days > 0 ? 'serán el' : days < 0 ? 'fueron el' : 'son hoy!'}
         <input type="date" value=${this.targetDate} @change=${this.onDateChanged} />
       </p>
+      ${copy}
     `
   }
 }
